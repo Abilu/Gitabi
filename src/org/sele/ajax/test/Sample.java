9eae1d81dc897bd68ba7b9acce88e59bd744527d
Abinaya <abi.vijaya04@gmail.com>
2020-02-25 11:14:19 +0530

    update

---
+++ src/org/sele/ajax/test/Sample.java
@@ -0,0 +1,12 @@
+package org.sele.ajax.test;
+
+public class Sample {
+	private void abi() {
+		System.out.println("good girl");
+	}
+	private void saran() {
+		System.out.println("good boy");
+
+	}
+
+}
